ac165a2588c51c6ea6d4a632f2a8ceed26d55b13
an920107 <an920107@gmail.com>
2024-06-20 11:20:48 +0800

    fix: sender id error.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import asyncio
 import ctypes
 from datetime import datetime
+import json
 from typing import Annotated
+
 from fastapi import Body, FastAPI, Response, WebSocket, WebSocketDisconnect
 from fastapi.concurrency import asynccontextmanager
 from fastapi.middleware.cors import CORSMiddleware
@@ -15,15 +17,16 @@ class Connection:
 
 
 class Message:
-    def __init__(self, room: str, sender: str, message: str):
+    def __init__(self, *, sender_id: int, room: str, sender: str, data: object):
         self.room = room
+        self.sender_id = sender_id
         self.sender = sender
-        self.content = message
+        self.content = data
 
 
 class SocketManager:
     def __init__(self):
-        self.connections: dict[str, list[Connection]] = {}
+        self.connections: dict[int, Connection] = {}
         self.queue = asyncio.Queue()
 
     async def connect(
@@ -32,28 +35,43 @@ class SocketManager:
         await websocket.accept()
         connection = Connection(websocket, room=room, name=name)
         await websocket.send_json({"id": id(connection)})
-        self.connections.setdefault(room, []).append(connection)
+        self.connections[id(connection)] = connection
         return connection
 
     def disconnect(self, connection: Connection) -> None:
-        self.connections.get(connection.room).remove(connection)
+        self.connections.pop(id(connection))
 
     async def broadcast(self, message: Message) -> None:
-        for connection in self.connections.setdefault(message.room, []):
-            await connection.websocket.send_json({
-                "id": id(connection),
-                "datetime": datetime.now().isoformat(),
-                "sender": message.sender,
-                "data": message.content,
-            })
+        for connection in self.connections.values():
+            if connection.room == message.room:
+                await connection.websocket.send_json(
+                    {
+                        "id": message.sender_id,
+                        "datetime": datetime.now().isoformat(),
+                        "sender": message.sender,
+                        "data": (
+                            message.content
+                            if type(message.content) is str
+                            else json.loads(json.dumps(message.content))
+                        ),
+                    }
+                )
 
     async def send_from_queue(self) -> None:
         while True:
             message = await self.queue.get()
             await self.broadcast(message)
 
-    async def add_message(self, message: Message) -> None:
-        await self.queue.put(message)
+    async def add_message(self, sender_id: str, data: object) -> None:
+        connection = self.connections[sender_id]
+        await self.queue.put(
+            Message(
+                sender_id=sender_id,
+                room=connection.room,
+                sender=connection.name,
+                data=data,
+            )
+        )
 
 
 manager = SocketManager()
@@ -85,20 +103,31 @@ def index():
 async def create_websocket(websocket: WebSocket, room: str, name: str):
     try:
         connection = await manager.connect(websocket, room=room, name=name)
-        await manager.add_message(Message(room, name, f"[{name} has joined]"))
+        await manager.add_message(id(connection), {
+            "event": "info",
+            "message": f"{name} has joined the room.",
+        })
         while True:
             await websocket.receive_text()
     except WebSocketDisconnect:
+        await manager.add_message(id(connection), {
+            "event": "info",
+            "message": f"{name} is leaving the room."
+        })
         manager.disconnect(connection)
-        await manager.add_message(Message(room, name, f"[{name} has left]"))
 
 
 @app.post("/broadcast")
 async def broadcast(id: int, message: Annotated[str, Body(embed=True)]):
     try:
-        connection: Connection = ctypes.cast(id, ctypes.py_object).value
-        await manager.add_message(Message(connection.room, connection.name, message))
-    except AttributeError | ValueError:
+        await manager.add_message(
+            id,
+            {
+                "event": "broadcast",
+                "message": message,
+            },
+        )
+    except KeyError:
         return Response(content=None, status_code=404)
     return Response(content=None, status_code=201)
 
